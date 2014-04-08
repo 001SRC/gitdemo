1f2db3da8fd6f41180c383e3df5f3575bdb58efd
CoffeeTimes <290618754@qq.com>
2014-04-08 17:28:56 +0800

    alt + shift +j 放在空白处，生成类的“注释”，放在方法名上，生成方法的注释

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,6 +1,14 @@
 
+/**
+ * @author sunwei
+ *
+ */
 public class HelloJava {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		System.out.println("the world of Java is beautiful .");
